47fcb473d006bbb4dccc80e0fcccd13f998b9cc1
Srkenway <rodrigostarkbr@gmail.com>
2019-02-21 20:23:44 -0300

    Initial commit

---
+++ src/Exemplos/catalogo.java
@@ -0,0 +1,32 @@
+package Exemplos;
+
+public class catalogo {
+
+    public static void main(String[] args) {
+        /*Estrutura de Seleção*/
+        int a = 10, b = 90, c = -100;
+        if (a > 0) {
+            a = c;
+        }
+        System.out.println(a);
+        if (c > a) {
+            c = a;
+        } else {
+            c = b;
+        }
+        System.out.println(c);
+        int op
+        int v
+        switch (op) {
+            case 1:
+                v = 1;
+            break;
+            case 2:
+                v = 2;
+            break;
+        default:
+            v = -1;
+        }
+        System.out.println( v );
+    }
+}
